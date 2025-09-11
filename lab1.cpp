735b1e3a41aa6e64ad83309f51bf3fb8b31e0d74
Владислав Дуничев <dunichevls1984@gmail.com>
2025-09-11 21:21:50 +0300

    Добавьте файлы проекта.

---
+++ lab1.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void Menu(Pipe t, KS k) {
+    int option;
+    while (true) {
+        cout << "Выберите действие:";
+        cout << "1 - добавить трубу";
+        cout << "2 - добавить компрессорную станцию \n";
+        cin >> option;
+        switch (option) {
+        case 1:
+            cout << "Insert pipe name: ";
+            cin >> t.name;
+            break;
+
+        };
+    };
+};
+
+struct Pipe {
+    string name;
+    float length;
+    int diametr;
+    bool repair;
+};
+struct KS {
+    string name;
+    int workshop_count;
+    int workshop_working;
+    string type;
+};
+
+int main()
+{
+    Pipe truba;
+    KS ks;
+    Menu(truba);
+
+}
+
